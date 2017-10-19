264de34e575dbd255a408a21f709f5765dfad62c
sakethkv <32929044+sakethkv@users.noreply.github.com>
2017-10-19 17:27:52 +0530

    Sucess

--- src/Generic/Lib.java
+++ src/Generic/Lib.java
@@ -1,5 +1,19 @@
 package Generic;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class Lib {
+	@Test
+	public static void RegisterUser()
+	{
+		Reporter.log("Register USer",true);
+	}
+
+	public static void DeleteUser()
+	{
+		Reporter.log("Delete USer",true);
+	}
+	
 
 }
